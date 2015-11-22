02b7d5d1de4655a464266c2c1bac720a2a2a0f8b
Michael Peterson <mikebean233@yahoo.com>
2015-11-21 21:13:32 -0800

    I figured out how to get threading working. We can use this to have a thread running in the background searching for new media files while the ui is still operating.

---
+++ Reference/Thread Testing/Thread Testing/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Thread_Testing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ScannerTest scannerTest = new ScannerTest(this);
+
+            ThreadStart threadStart = new ThreadStart(scannerTest.RunTest);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+            //this.Dispatcher.Invoke(new Action(() => (new ScannerTest(this)).RunTest()), new object[] { });
+        }
+
+        public void AppendText(string newText)
+        {
+            this.Dispatcher.Invoke(new Action(() => this.textBlock.Text += newText), new object[] { });
+            
+        }
+
+    }
+}
---
+++ Reference/Thread Testing/Thread Testing/ScannerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread_Testing
+{
+    class ScannerTest
+    {
+        private MainWindow _parentWindow;
+
+        public ScannerTest(MainWindow parentWindow)
+        {
+            _parentWindow = parentWindow;
+        }
+
+        public void RunTest()
+        {
+            for (int i = 0; i < 20; ++i)
+            {
+                _parentWindow.Dispatcher.Invoke(new Action(() => _parentWindow.AppendText(" " + i + " ")), new object[] { });
+                
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+
+    }
+}
