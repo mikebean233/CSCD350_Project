2170763b451b91c331804b3de11239565171999e
Michael Peterson <mikebean233@yahoo.com>
2015-11-22 00:33:49 -0800

    Stubbed out a Main controller and  connections between other components

---
+++ BynarsMediaPlayer/MediaPlayer/MediaPlayer/FileScannerStub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayer
+{
+    class FileScannerStub
+    {
+        private MainController _mainController;
+
+        public FileScannerStub(MainController mainController)
+        {
+            _mainController = mainController;
+        }
+
+        public void ScanDirectory(object path)
+        {
+            // add some pretend files to the library
+            _mainController.AddMediaEvent("C:\\PretendFile1.mp3");
+            _mainController.AddMediaEvent("C:\\PretendFile2.mp3");
+        }
+
+        
+
+    }
+}
--- BynarsMediaPlayer/MediaPlayer/MediaPlayer/MainWindow.xaml.cs
+++ BynarsMediaPlayer/MediaPlayer/MediaPlayer/MainWindow.xaml.cs
@@ -26,58 +26,38 @@ namespace MediaPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
-       //Sound Control
-        uint curVolume = 0;
-        [DllImport("winmm.dll")]
-        public static extern int waveOutGetVolume(IntPtr hwo, out uint dwVolume);
-        [DllImport("winmm.dll")]
-        public static extern int waveOutSetVolume(IntPtr hwo, uint dwVolume);
-
+        private MainController _mainController;
         public MainWindow()
         {
             InitializeComponent();
+
+            _mainController = new MainController(this);
         }
 
         private void poly_PlayButton_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            //lbl_MetaData.Content = curMedia.getMetaData();
-            //prgBar_ScrubBar.Value = playedTime * 100;
-            //lbl_ScrubBarTime.Content = "" playedTime + " / " + totalTime;
+            this.Dispatcher.Invoke(new Action(() => _mainController.PlayButtonPressed()), new object[] { });
         }
 
         private void poly_StopButton_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            this.Dispatcher.Invoke(new Action(() => _mainController.StopButtonPressed()), new object[] { });
         }
 
-        private void poly_SkipForeward_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-        }
-
-        private void poly_SkipBackward_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-        }
-
+        
         private void slider_VolumeControl_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            //Calculate Volume
-            int newVolume = (int)((ushort.MaxValue / 5) * slider_VolumeControl.Value);
+            this.Dispatcher.Invoke(new Action(() => _mainController.VolumeChanged()), new object[] { });
+        }
 
-            //Set Volume for all channels
-            uint NewVolumeAllChannels = (((uint)newVolume & 0x0000ffff) | ((uint)newVolume << 16));
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            (new Thread(new ThreadStart(_mainController.Setup))).Start();
+        }
 
-            //Set the Volume
-            waveOutSetVolume(IntPtr.Zero, NewVolumeAllChannels);
-
-            //Image Change
-            if (newVolume == 0)
-            {
-                //rect_VolumeImage.Fill = imgBrushVolumeOff;
-            }
-
-            else
-            {
-                //rect_VolumeImage.Fill = imgBrushVolumeOn;
-            }
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            this.Dispatcher.Invoke(new Action(() => _mainController.CloseWindow()), new object[] { });
         }
     }
 }
